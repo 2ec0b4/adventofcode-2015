95caf9259fb74ef6abd26c0a780c61fd16ff9b67
Antoine <antoine@2ec0b4.fr>
2015-12-07 23:27:49 +0100

    WIP

--- Day-6/step1.php
+++ Day-6/step1.php
@@ -4,5 +4,17 @@ require_once __DIR__ . '/common.php';
 
 $instructions   = explode("\n", $input);
 foreach($instructions as $instruction) {
+    $matches    = [];
+    preg_match('@^([a-z ]+) ([0-9]+),([0-9]+) through ([0-9]+),([0-9]+)$@im', $instruction, $matches);
     
+    switch($matches[1]) {
+        case 'toggle':
+            break;
+        
+        case 'turn off':
+            break;
+        
+        case 'turn off':
+            break;
+    }
 }
