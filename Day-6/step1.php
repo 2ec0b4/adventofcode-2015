27d3c434a9ecf2afbec4375d6efa4bb3dec22d9b
Antoine <antoine@2ec0b4.fr>
2015-12-08 09:51:22 +0100

    Day 6 solutions - step 1

--- Day-6/step1.php
+++ Day-6/step1.php
@@ -2,19 +2,37 @@
 
 require_once __DIR__ . '/common.php';
 
+$grid           = [];
 $instructions   = explode("\n", $input);
 foreach($instructions as $instruction) {
     $matches    = [];
     preg_match('@^([a-z ]+) ([0-9]+),([0-9]+) through ([0-9]+),([0-9]+)$@im', $instruction, $matches);
     
-    switch($matches[1]) {
-        case 'toggle':
-            break;
-        
-        case 'turn off':
-            break;
-        
-        case 'turn off':
-            break;
+    for($i=$matches[2];$i<=$matches[4];$i++) {
+        for($j=$matches[3];$j<=$matches[5];$j++) {
+            switch($matches[1]) {
+                case 'toggle':
+                    if( !isset($grid[$i][$j]) ) {
+                        $grid[$i][$j]   = 1;
+                        break;
+                    }
+                case 'turn off':
+                    unset($grid[$i][$j]);
+                    break;
+
+                case 'turn on':
+                    $grid[$i][$j]   = 1;
+                    break;
+            }
+        }
     }
 }
+
+$count  = 0;
+for($i=0;$i<=999;$i++) {
+    if( isset($grid[$i]) ) {
+        $count  += sizeof($grid[$i]);
+    }
+}
+
+echo 'Answer: '.$count;
