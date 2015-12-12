6fd102548b2d1db216a28f60c10b1d47a12f021e
Antoine <antoine@2ec0b4.fr>
2015-12-12 22:40:17 +0100

    Symplify a function

--- Day-12/common.php
+++ Day-12/common.php
@@ -10,9 +10,6 @@ $input  = file_get_contents($filename);
 function walkAndSum($data, &$sum=0, $filter=false) {
     if( is_numeric($data) ) {
         $sum    += intval($data);
-        return $sum;
-    } else if( is_string($data) ) {
-        return $sum;
     } else if( is_object($data) ) {
         $data   = get_object_vars($data);
         if( $filter && in_array('red', $data, true) ) {
@@ -20,8 +17,10 @@ function walkAndSum($data, &$sum=0, $filter=false) {
         }
     }
     
-    foreach($data as $value) {
-        walkAndSum($value, $sum, $filter);
+    if( is_array($data) ) {
+        foreach($data as $value) {
+            walkAndSum($value, $sum, $filter);
+        }
     }
     
     return $sum;
